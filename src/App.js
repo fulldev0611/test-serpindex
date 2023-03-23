25f7ee1d30977f0424c386a213f66e5523d8439f
Farihul Rouf <farihulrouf@gmail.com>
2023-03-23 23:29:41 +0700

    init 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import BootstrapTable from 'react-bootstrap-table-next';
+import Pagination from 'react-bootstrap-table2-paginator';
+import cellEditFactory, {Type} from 'react-bootstrap-table2-editor'
+import filterFactory, {textFilter} from 'react-bootstrap-table2-filter';
 
 function App() {
+  const [ data, setData] = useState([])
+  useEffect(() => {
+    getData()
+  }, [])
+  const getData= () => {
+    axios("https://serpindex-demo.svc.violetvault.com/api/index").then((res) => 
+      setData(res.data)
+    )
+  }
+
+  const columns= [
+   {
+     dataField: "title",
+      text: "Title",
+      sort: true,
+   },
+   {
+    dataField: "Cetegory",
+    text: "cetegory",
+    sort: true,
+   },
+   {
+    dataField: "domain",
+    text: "Domain",
+    sort: true,
+    filter: textFilter(),
+   }
+  ]
+
+  console.log(data)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <BootstrapTable
+        keyField='id'
+         data={data} 
+         columns={columns} 
+         striped
+         hover
+         condensed
+         pagination={Pagination()}
+         cellEdit={cellEditFactory({
+            mode: "click",
+         })}
+         filter={filterFactory()}
+        />
+        
     </div>
   );
 }
