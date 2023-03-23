73a52e2b8a1fffbf5a059e27767a462991150872
Farihul Rouf <farihulrouf@gmail.com>
2023-03-24 00:57:08 +0700

    init 12

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import BootstrapTable from 'react-bootstrap-table-next';
 import Pagination from 'react-bootstrap-table2-paginator';
-import cellEditFactory, {Type} from 'react-bootstrap-table2-editor'
+//import cellEditFactory, {Type} from 'react-bootstrap-table2-editor'
+import ToolkitProvider, {Search} from 'react-bootstrap-table2-toolkit/dist/react-bootstrap-table2-toolkit';
+
 import filterFactory, {textFilter} from 'react-bootstrap-table2-filter';
 import { Modal, Button } from 'react-bootstrap';
 function App() {
+  const {SearchBar} = Search;
   const [ data, setData] = useState([])
   const [modalInfo, setmodalInfo] = useState([])
   const [showModal, setShowModal] = useState(false)
@@ -44,8 +47,8 @@ function App() {
     dataField: "domain",
     text: "Domain",
     sort: true,
-    filter: textFilter(),
    }
+   
   ]
   const rowEvents = {
     onClick: (e,row) => {
@@ -89,21 +92,28 @@ function App() {
   console.log(data)
   return (
     <div className="App">
-      
-      <BootstrapTable
-        keyField='id'
-         data={data} 
-         columns={columns} 
-         rowEvents={rowEvents}
-         striped
-         hover
-         condensed
-         pagination={Pagination({})}
-         //cellEdit={cellEditFactory({
-          //  mode: "click",
-         //})}
-         filter={filterFactory()}
-        />
+      <ToolkitProvider
+          keyField="id"
+          data={ data }
+          columns={ columns }
+          search
+    >
+       {
+      props => (
+        <div>
+          <SearchBar { ...props.searchProps } />
+          <hr />
+          <BootstrapTable
+           rowEvents={rowEvents}
+
+            { ...props.baseProps }
+          />
+        </div>
+      )
+    }
+
+  </ToolkitProvider>
+     
         {show ? <ModalContent /> : null}
     </div>
   );
